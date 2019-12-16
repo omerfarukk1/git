33a74a42132bdf1a9d45690bc78a6641eca81852
Omer faruk Karacam <redox1972@gmail.com>
2019-12-16 20:21:25 +0000

    Same line changes

--- src/test/java/tests/login.java
+++ src/test/java/tests/login.java
@@ -2,12 +2,12 @@ package tests;
 
 public class login {
     public static void main(String[] args) {
-        
-        
-        
-        
-        
-        
+
+
+
+
+
+        System.out.println("Hello local repository");
         
         
         //this is github
